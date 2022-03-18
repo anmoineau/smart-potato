10d633e9273ee4f01b889e161cfa096534c2d3ed
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-18 10:59:14 +0100

    Add Recipe's tests in MainViewModel

--- smart-potato/MVVM/ViewModel/MainViewModel.cs
+++ smart-potato/MVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SmartPotato.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,20 @@ namespace SmartPotato.MVVM.ViewModel
 {
     internal class MainViewModel
     {
+        public MainViewModel()
+        {
+            Recipe recipe = new(1)
+            {
+                Name = "Smart potatoes",
+                Ingredients = "Potatoes",
+                Instructions = "Boil them.",
+                ExpiryIndex = ExpiryIndexes.NORMAL,
+                Season = Seasons.ANY,
+                LastMade = DateTime.Now
+            };
+            Seasons HotSeasons = Seasons.SUMMER | Seasons.SPRING;
+            Debug.WriteLine(recipe.ToString());
+            Debug.WriteLine(recipe.Season.HasFlag(HotSeasons));
+        }
     }
 }
