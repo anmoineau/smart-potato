d7f704baa93d28fabbfa6de934d16180be89a900
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-22 15:19:19 +0100

    Add base menu list

--- smart-potato/MVVM/ViewModel/MenuViewModel.cs
+++ smart-potato/MVVM/ViewModel/MenuViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SmartPotato.Core;
+using SmartPotato.MVVM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace SmartPotato.MVVM.ViewModel
 {
-    internal class MenuViewModel
+    internal class MenuViewModel  :ObservableObject
     {
+        private List<Meal>? menu;
+
+        public List<Meal>? Menu
+        {
+            get { return menu; }
+            set { menu = value; OnPropertyChanged(); }
+        }
+
+        public MenuViewModel()
+        {
+            Menu = MenuHandler.Menu;
+            MenuHandler.RenewMenu();
+        }
+
     }
 }
