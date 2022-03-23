15f922eca6854cfbe5865c02b1d77eeaa5d0295e
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-23 10:09:36 +0100

    Add PlanningView base UI

--- smart-potato/MVVM/ViewModel/PlanningViewModel.cs
+++ smart-potato/MVVM/ViewModel/PlanningViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SmartPotato.Core;
+using SmartPotato.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,24 @@ namespace SmartPotato.MVVM.ViewModel
 {
     internal class PlanningViewModel
     {
+        public ObservableCollection<Recipe> RecipesDone { get; set; }
+        public ObservableCollection<Recipe> RecipesTodo { get; set; }
+
+        /**** Relay Commands ****/
+
+        /*** CanExecute ***/
+
+        /**** Constructor ****/
+        public PlanningViewModel()
+        {
+            InitializeViewCommands();
+            RecipesDone = MenuHandler.GetInstance.RecipesDone;
+            RecipesTodo = MenuHandler.GetInstance.RecipesTodo;
+        }
+
+        /**** Methods ****/
+        private void InitializeViewCommands()
+        {
+        }
     }
 }
