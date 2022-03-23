8527f54ec9239751a8d50c38321f43da45caef43
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-23 14:02:09 +0100

    Add AppData explorer to Parameters

--- smart-potato/MVVM/ViewModel/ParametersViewModel.cs
+++ smart-potato/MVVM/ViewModel/ParametersViewModel.cs
@@ -2,6 +2,8 @@
 using SmartPotato.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +18,15 @@ namespace SmartPotato.MVVM.ViewModel
             set { TimeProvider.CurrentTime = value; OnPropertyChanged(); }
         }
 
+        public string AppDataPath { get; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"SmartPotato\");
 
         /**** Relay Commands ****/
         public RelayCommand? AddDaysCommand { get; set; }
+        public RelayCommand? OpenAppDataCommand { get; set; }
 
         /*** CanExecute ***/
         private bool isAddDaysEnabled = true;
+        private bool isOpenAppDataEnabled = true;
 
         /**** Constructor ****/
         public ParametersViewModel()
@@ -37,6 +42,11 @@ namespace SmartPotato.MVVM.ViewModel
                 TimeProvider.AddDays(int.Parse(o.ToString()?? "0"));
                 OnPropertyChanged("CurrentTime");
             }, canExecute => isAddDaysEnabled);
+
+            OpenAppDataCommand = new RelayCommand(o =>
+            {
+                Process.Start("explorer.exe", AppDataPath);
+            }, canExecute => isOpenAppDataEnabled);
         }
     }
 }
