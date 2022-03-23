4dd5c86cbeebd47eab865a4bd7908bd7bc7e07c5
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-23 12:22:41 +0100

    Add basic date handling in ParametersView

--- smart-potato/MVVM/ViewModel/ParametersViewModel.cs
+++ smart-potato/MVVM/ViewModel/ParametersViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SmartPotato.Core;
+using SmartPotato.MVVM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,35 @@ using System.Threading.Tasks;
 
 namespace SmartPotato.MVVM.ViewModel
 {
-    internal class ParametersViewModel
+    internal class ParametersViewModel : ObservableObject
     {
+        public DateTime CurrentTime
+        {
+            get { return TimeProvider.CurrentTime; }
+            set { TimeProvider.CurrentTime = value; OnPropertyChanged(); }
+        }
+
+
+        /**** Relay Commands ****/
+        public RelayCommand? AddDaysCommand { get; set; }
+
+        /*** CanExecute ***/
+        private bool isAddDaysEnabled = true;
+
+        /**** Constructor ****/
+        public ParametersViewModel()
+        {
+            InitializeViewCommands();
+        }
+
+        /**** Methods ****/
+        private void InitializeViewCommands()
+        {
+            AddDaysCommand = new RelayCommand(o =>
+            {
+                TimeProvider.AddDays(int.Parse(o.ToString()?? "0"));
+                OnPropertyChanged("CurrentTime");
+            }, canExecute => isAddDaysEnabled);
+        }
     }
 }
