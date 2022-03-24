0a9cf0bf309eca13c7174d71660451cef5fd5901
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-24 12:24:05 +0100

    Add mechanism to close Recipe windows

--- smart-potato/MVVM/ViewModel/RecipeViewModel.cs
+++ smart-potato/MVVM/ViewModel/RecipeViewModel.cs
@@ -1,19 +1,41 @@
-﻿using SmartPotato.MVVM.Model;
+﻿using SmartPotato.Core;
+using SmartPotato.MVVM.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SmartPotato.MVVM.ViewModel
 {
     internal class RecipeViewModel
     {
+        /**** Properties ****/
         public Recipe CurrentRecipe { get; set; }
+
+        /**** RelayCommand ****/
+        public RelayCommand? CloseWindowCommand { get; set; }
+
+        /*** CanExecute ***/
+        private bool isCloseWindowEnabled = true;
+
+        /**** Constructor ****/
         public RecipeViewModel(uint UID)
         {
+            InitializeViewCommands();
             List<Recipe> recipes = MenuHandler.GetInstance.RecipeBook;
             CurrentRecipe = recipes.Find(recipe => recipe.UID == UID) ?? new();
+        }
+
+        /**** Methods ****/
+        private void InitializeViewCommands()
+        {
+            CloseWindowCommand = new RelayCommand(o =>
+            {
+                if (o != null)
+                    ((Window)o).Close();
+            }, canExecute => isCloseWindowEnabled);
         }
     }
 }
