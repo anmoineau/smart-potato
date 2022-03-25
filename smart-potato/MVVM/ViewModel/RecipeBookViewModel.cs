2f4e1f1437eb183813d3fceb30669912d42a7934
Antoine MOINEAU <antoine.moineau@thalesgroup.com>
2022-03-25 10:31:48 +0100

    Add recipe book list implementation

--- smart-potato/MVVM/ViewModel/RecipeBookViewModel.cs
+++ smart-potato/MVVM/ViewModel/RecipeBookViewModel.cs
@@ -12,6 +12,7 @@ namespace SmartPotato.MVVM.ViewModel
     {
         /**** Properties ****/
         public ObservableCollection<Recipe> RecipeBook { get; set; }
+        public ObservableCollection<Tuple<Recipe, Recipe?>> RecipeBookFormat { get; set; } = new();
 
         /**** Relay Commands ****/
 
@@ -22,11 +23,29 @@ namespace SmartPotato.MVVM.ViewModel
         {
             InitializeViewCommands();
             RecipeBook = MenuHandler.GetInstance.RecipeBook;
+            FormatRecipeBook();
         }
 
         /**** Methods ****/
         private void InitializeViewCommands()
         {
+        }
+
+        private void FormatRecipeBook()
+        {
+            for (int i = 0; i < RecipeBook.Count; i+=2)
+            {
+                Tuple<Recipe, Recipe?> items;
+                if (i+1 >= RecipeBook.Count)
+                {
+                    items = new(RecipeBook[i], null);
+                }
+                else
+                {
+                    items = new(RecipeBook[i], RecipeBook[i + 1]);
+                }
+                RecipeBookFormat.Add(items);
+            }
         }
     }
 }
